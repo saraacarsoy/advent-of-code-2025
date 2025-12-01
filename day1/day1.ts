86b797dbde6284e51986cd407ad128983407ec74
sacarsoy <saraacarsoy@sabanciuniv.edu>
2025-12-01 15:57:15 +0100

    make p1 nicer

--- day1/day1.ts
+++ day1/day1.ts
@@ -9,23 +9,12 @@ function iterateLines() {
   	for (const line of lines) {
 			const dir = line[0];
 			let amount = Number(line.slice(1));
+
 			if (dir === "L") amount = -amount;
-			const next = (curr + amount) % 100;
+			
+			curr = (curr + amount) % 100;
 
-			if (next < 0) {
-				curr = 100 + next;
-			}
-
-			else if (next > 0 && next < 100) {
-				curr = next;
-			}
-
-			else if (next > 100) {
-				curr = next - 100;
-			}
-
-			else if (next == 0 || next == 100) {
-				curr = 0;
+			if (curr == 0) {
 				zeroCounter += 1;
 			}
     }
