37298f2ef66b4178a9fe9df122c5737987f87eb8
sara <saraacarsoy@sabanciuniv.edu>
2025-12-01 19:13:16 +0100

    add d1 p1 p2

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,4 +1,40 @@
-import { parseGrid } from "../utils/aoc";
+import { readLines } from "../utils/aoc";
 
-const lines = parseGrid("./day1.txt");
-console.log(lines);
+const lines = readLines("./day1.txt");
+
+let curr = 50;
+let zeroCounter = 0;
+let tickCounter = 0;
+
+function iterateLines() {
+    for (const line of lines) {
+        const dir = line[0];
+        let amount = Number(line.slice(1));
+
+        if (dir === "L") amount = -amount;
+
+        p2(amount);
+
+        const next = curr + amount;
+        curr = ((next % 100) + 100) % 100;
+        if (curr === 0) zeroCounter += 1;
+    }
+}
+
+function p2(amount: number) {
+    const steps = Math.abs(amount);
+    const stepDir = amount > 0 ? 1 : -1;
+
+    let firstPass = stepDir === 1 ? (100 - curr) % 100 : curr;
+    let passes = 0;
+
+    if (steps > firstPass) {
+        passes = 1 + Math.floor((steps - firstPass - 1) / 100);
+    }
+
+    tickCounter += passes;
+}
+
+iterateLines();
+console.log("Ends at 0: ", zeroCounter); // 1135
+console.log("Ticks at 0: ", tickCounter); // 6558
