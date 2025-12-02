7bdde94fc6bde2936db12ff80810069c7b20acc3
sara <saraacarsoy@sabanciuniv.edu>
2025-12-02 19:57:18 +0100

    update part 1 to remove restrictions

--- day2/day2.ts
+++ day2/day2.ts
@@ -7,34 +7,22 @@ function iterateintervals() {
     for (const interval of intervals) {
         const limits: string[] = interval.split('-');
 
-        if (shouldCheck(limits[0], limits[1])) {
-            let lowerInterval = limits[0];
-            let higherLimit = limits[1];
+        let lowerInterval = limits[0];
+        let higherLimit = limits[1];
 
-            // no need to check equality as input is safe for 10 = 10 or 99 = 99
-            if (limits[0].length % 2 !== 0) {
-                const evenDigit = rollToHigherEvenDigitCount(lowerInterval);
-                if (Number(evenDigit) < Number(limits[1])) {
-                    lowerInterval = evenDigit;
-                }
-            }
-            if (limits[1].length % 2 !== 0) {
-                const evenDigit = rollToLowerEvenDigitCount(higherLimit);
-                if (Number(evenDigit) > Number(limits[0])) {
-                    higherLimit = evenDigit;
-                }
-            }
-
-            checkRepetition(lowerInterval, Number(higherLimit))
-        }
+        checkRepetition(lowerInterval, Number(higherLimit))
     }
 }
 
 function checkRepetition(lowerLimit: string, higherLimit: number) {
     const digits = lowerLimit.length;
-    const repetitiveDigits = digits / 2;
+    const repetitiveDigits = Math.ceil(digits / 2);
 
-    const low = Number(lowerLimit);
+    if (digits % 2 !== 0) {
+        lowerLimit = rollToHigherEvenDigitCount(lowerLimit);
+    }
+
+    let low = Number(lowerLimit);
     const high = Number(higherLimit);
 
     const half = lowerLimit.slice(0, repetitiveDigits);
@@ -53,25 +41,9 @@ function checkInterval(lower: number, higher: number, n: string) {
     }
 }
 
-function shouldCheck(lower: string, higher: string): boolean {
-    // if both lower and higher interval same digit and it is an odd number, skip
-    const digits = lower.length;
-
-    if (digits === higher.length && digits % 2 !== 0) {
-        return false;
-    }
-
-    return true;
-}
-
 function rollToHigherEvenDigitCount(n: string): string {
     const digits = n.length;
     return "1" + "0".repeat(digits);
-}
-
-function rollToLowerEvenDigitCount(n: string): string {
-    const digits = n.length;
-    return "9".repeat(digits - 1);
 }
 
 function getDivisorsOfDigit(num: string): number[] {
