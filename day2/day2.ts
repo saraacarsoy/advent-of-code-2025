280f529a8f8eaeb2de047d252d3dfe6ca8db98dd
sara <saraacarsoy@sabanciuniv.edu>
2025-12-02 20:33:20 +0100

    rewrite part1 again

--- day2/day2.ts
+++ day2/day2.ts
@@ -10,57 +10,27 @@ function iterateintervals() {
         let lowerInterval = limits[0];
         let higherLimit = limits[1];
 
-        checkRepetition(lowerInterval, Number(higherLimit))
-    }
-}
-
-function checkRepetition(lowerLimit: string, higherLimit: number) {
-    const digits = lowerLimit.length;
-    const repetitiveDigits = Math.ceil(digits / 2);
-
-    if (digits % 2 !== 0) {
-        lowerLimit = rollToHigherEvenDigitCount(lowerLimit);
-    }
-
-    let low = Number(lowerLimit);
-    const high = Number(higherLimit);
-
-    const half = lowerLimit.slice(0, repetitiveDigits);
-    const halfAsInt = Number(half);
-
-    for (let i = halfAsInt; i < Number(rollToHigherEvenDigitCount(half)); i++) {
-        checkInterval(low, high, i.toString());
-    }
-}
-
-function checkInterval(lower: number, higher: number, n: string) {
-    let duplicate = Number(n + n);
-
-    if (lower <= duplicate && higher >= duplicate) {
-        counter += duplicate;
-    }
-}
-
-function rollToHigherEvenDigitCount(n: string): string {
-    const digits = n.length;
-    return "1" + "0".repeat(digits);
-}
-
-function getDivisorsOfDigit(num: string): number[] {
-    const digits = num.length;
-    const half = digits / 2;
-
-    const divisors: number[] = [1];
-
-    for (let i = 2; i < half; i++) {
-        if (digits % i === 0 && i !== half) {
-            divisors.push(i);
+        for (let i = Number(lowerInterval); i <= Number(higherLimit); i++) {
+            if (isInvalidPart1(i)) {
+                counter += i;
+            }
         }
     }
 
-    return divisors;
+    console.log(counter)
 }
 
+function isInvalidPart1(n: number): boolean {
+    const s = String(n);
+    const len = s.length;
+
+    if (len % 2 !== 0) return false;
+
+    const half = len / 2;
+    const prefix = s.slice(0, half);
+    const repeated = prefix + prefix;
+
+    return repeated === s;
+}
 
 iterateintervals();
-console.log(counter)
