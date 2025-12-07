9c107cec04c6877714ae170bdaac5c8cd3c77556
sara <saraacarsoy@sabanciuniv.edu>
2025-12-07 15:50:59 +0100

    day7 p1

--- day7/day7.ts
+++ day7/day7.ts
@@ -9,35 +9,41 @@ queue.push(start);
 let count = 0;
 let visited: [number, number][] = [];
 
-while (queue.length > 0) {
-    const [row, col] = queue.shift()!;
+function part1() {
+    while (queue.length > 0) {
+        const [row, col] = queue.shift()!;
 
-    if (
-        row + 1 < grid.length &&
-        grid[row + 1][col] === '.' &&
-        !visited.some(([r, c]) => r === row + 1 && c === col)
-    ) {
-        queue.push([row + 1, col]);
-        visited.push([row + 1, col]);
-    } else {
-        if (
-            row + 1 < grid.length && 
-            col + 1 < grid[0].length &&
-            !visited.some(([r, c]) => r === row + 1 && c === col + 1)
-        ) {
-            queue.push([row + 1, col + 1]);
-            visited.push([row + 1, col + 1]);
-            count += 1;
-        }
         if (
             row + 1 < grid.length &&
-            col - 1 >= 0 &&
-            !visited.some(([r, c]) => r === row + 1 && c === col - 1)
+            grid[row + 1][col] === '.' &&
+            !visited.some(([r, c]) => r === row + 1 && c === col)
         ) {
-            queue.push([row + 1, col - 1]);
-            visited.push([row + 1, col - 1]);
+            queue.push([row + 1, col]);
+            visited.push([row + 1, col]);
+        } else if (row + 1 < grid.length && grid[row + 1][col] === '^') {
             count += 1;
+
+            if (
+                row + 1 < grid.length &&
+                col + 1 < grid[0].length &&
+                !visited.some(([r, c]) => r === row + 1 && c === col + 1)
+            ) {
+                queue.push([row + 1, col + 1]);
+                visited.push([row + 1, col + 1]);
+            }
+            if (
+                row + 1 < grid.length &&
+                col - 1 >= 0 &&
+                !visited.some(([r, c]) => r === row + 1 && c === col - 1)
+            ) {
+                queue.push([row + 1, col - 1]);
+                visited.push([row + 1, col - 1]);
+            }
         }
     }
+    console.log(count);
 }
-console.log(count)
+
+// part1();
+
+function part2() {}
